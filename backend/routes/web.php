5efa67115fef9bad9c3e0eba6b4e9225e236f9d7
Nasgoruru <arrofiryanto@gmail.com>
2025-08-25 20:24:03 +0700

    tambah route,controller dan benahi galeri

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\GaleriController;
 
 Route::get('/', function () {
     return view('main.index');
+})->name('home');
+
+Route::prefix('galeri')->group(function () {
+    Route::get('/galeri', [GaleriController::class, 'index'])->name('galeri.index');
+    Route::get('/galeri{id}', [GaleriController::class, 'show'])->name('galeri.show');
+
 });
+
+Route::get('/main', [GaleriController::class, 'index'])->name('main.index');
+
+
